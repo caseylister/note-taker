7aa8e72a0ef1e81b4014d0257c40c8b91c0ff7bd
caseylister <caseynlister@gmail.com>
2021-11-20 14:38:31 -0500

    update format

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require("fs");
+const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
